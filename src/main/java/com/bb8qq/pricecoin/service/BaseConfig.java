1429c965868402a35559468061e7ea5cfb568dcc
pbadun <pbadun@gmail.com>
2022-07-13 16:45:05 +0300

    разгрузка параметров из конфига

--- src/main/java/com/bb8qq/pricecoin/service/BaseConfig.java
+++ src/main/java/com/bb8qq/pricecoin/service/BaseConfig.java
@@ -3,12 +3,19 @@ package com.bb8qq.pricecoin.service;
 import com.bb8qq.pricecoin.model.Coin;
 import com.bb8qq.pricecoin.repo.CoinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component
 public class BaseConfig {
+
+    @Value("${coin.id}")
+    private String ids;
+
+    @Value("${coin.symbol}")
+    private String symbols;
 
     @Autowired
     private CoinRepository coinRepository;
@@ -19,12 +26,12 @@ public class BaseConfig {
         if (total != 0) {
             return;
         }
-        int[] ids = {90, 80, 48543};
-        String[] symbols = {"BTC", "ETH", "SOL"};
-        for (int i = 0; i < ids.length; i++) {
+        String[] id = ids.split(",");
+        String[] symbol = symbols.split(",");
+        for (int i = 0; i < id.length; i++) {
             Coin c = new Coin();
-            c.setIdCoin(ids[i]);
-            c.setSymbolCoin(symbols[i]);
+            c.setIdCoin(Integer.parseInt(id[i]));
+            c.setSymbolCoin(symbol[i]);
             coinRepository.save(c);
         }
     }
